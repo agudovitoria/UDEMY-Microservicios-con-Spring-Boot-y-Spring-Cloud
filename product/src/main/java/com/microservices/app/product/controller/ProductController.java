327c2d0c0d55725c73f0a78c97a87fc38e8657ce
Iñaki Agudo <agudovitoria@gmail.com>
2020-04-04 20:51:29 +0200

    Apply Hystrix configuration into Product service
    Set desired log level

--- product/src/main/java/com/microservices/app/product/controller/ProductController.java
+++ product/src/main/java/com/microservices/app/product/controller/ProductController.java
@@ -4,13 +4,15 @@ import com.microservices.app.product.persistence.entity.Product;
 import com.microservices.app.product.service.IProductService;
 import java.util.List;
 import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("products")
+@RequestMapping("/")
+@Slf4j
 public class ProductController {
 
   private final IProductService service;
@@ -21,11 +23,13 @@ public class ProductController {
 
   @GetMapping()
   public List<Product> getAll() {
+    log.debug("ProductController.getAll()");
     return service.findAll();
   }
 
   @GetMapping("{id}")
   public Product getById(@PathVariable UUID id) {
+    log.debug(String.format("ProductController.getById(%s) fallback", id.toString()));
     return service.findById(id);
   }
 }
